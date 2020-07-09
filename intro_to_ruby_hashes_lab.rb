71cf27cc945855c0c8fd066f54aa40fe7de2d5e9
Ynda Jas <yndajas@gmail.com>
2020-07-09 16:11:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -56,7 +56,9 @@ def monopoly_with_fourth_tier
 	      b_and_o_railroad: {
 	        "mortgage_value" => "$400"
 	      },
-	      shortline: {}
+	      shortline: {
+	        "mortgage_value" => "$800"
+	      }
 	    },
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
