81fcccd41873c7e7c7390cea2ba66eba26a0516b
Ynda Jas <yndajas@gmail.com>
2020-07-09 16:09:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,8 @@ def monopoly_with_third_tier
 	    pieces: 4,
 	    names: {
 	      reading_railroad: {},
-	      pennsylvania_railroad: {}
+	      pennsylvania_railroad: {},
+	      b_and_o_railroad: {}
 	    },
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
